d3faa3c42d1a0cd92e58aceeb9c1bb221a352678
Mena Erian <mena3koko2karas1@gmail.com>
2025-09-04 21:21:24 +0300

    Part 1 , instal EF and tools, Make Reverse Enginaring, and see the overall of EF

---
+++ Demo/Models/EmployeeConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public partial class Employee
+    {
+        //public override string ToString() =>
+        //    $"ID:{BusinessEntityID}, NationalID:{NationalIDNumber}, Login:{LoginID}, Title:{JobTitle}, OrgLevel:{OrganizationLevel?.ToString() ?? "N/A"}, Birth:{BirthDate:yyyy-MM-dd}, Marital:{MaritalStatus}, Gender:{Gender}, Hire:{HireDate:yyyy-MM-dd}, Salaried:{SalariedFlag}, Vacation:{VacationHours}, Sick:{SickLeaveHours}, Status:{(CurrentFlag ? "Active" : "Inactive")}, GUID:{rowguid}, Modified:{ModifiedDate:yyyy-MM-dd HH:mm:ss}";
+
+    }
+}
